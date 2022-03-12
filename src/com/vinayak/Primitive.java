5663e25fa167a1e773ff34c74eea44d8fbfb4a91
Vinayak Mishra <vinayakmishra26@gmail.com>
2022-03-12 23:42:58 +0530

    FINAL

---
+++ src/com/vinayak/Primitive.java
@@ -0,0 +1,12 @@
+package com.vinayak;
+
+public class Primitive {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.67f;
+        double largeDecimalNumbers = 456789.4567;
+        long largeInteger = 34566677889L;
+        boolean check = true;
+    }
+}

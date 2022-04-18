061c3ff392618459a0b4a831fc2b557fc673ebe0
Vinayak Mishra <vinayakmishra26@gmail.com>
2022-04-18 12:51:53 +0530

    init

---
+++ datatypes.java
@@ -0,0 +1,11 @@
+public class datatypes {
+    public static void main(String[] args) {
+        int age = 22;
+        String name = "Vinayak";
+        float weight = 70.5f;
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(weight);
+
+    }
+}
